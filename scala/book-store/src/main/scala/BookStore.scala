7358169d2d615bf78f5ada9dd85849538a11c841
Basavaraj Kalloli <scalolli@users.noreply.github.com>
2018-10-16 22:19:31 +0100

    Complete the book store exercise yay, feels really good! 😀😀

---
+++ scala/book-store/src/main/scala/BookStore.scala
@@ -0,0 +1,60 @@
+object BookStore {
+
+  def total(quantityOfBooks: List[Int]): Int = {
+    if (quantityOfBooks.isEmpty)
+      0
+    else {
+      val prices = groupBooks(quantityOfBooks).map { group =>
+        group.books.map(l => price(l.size)).sum
+      }
+      prices.min
+    }
+  }
+
+  private val bookPrice = 800
+  def price(size: Int): Int =
+    (size match {
+      case 1 => bookPrice
+      case 2 => (2 * bookPrice * 0.95)
+      case 3 => (3 * bookPrice * 0.90)
+      case 4 => (4 * bookPrice * 0.80)
+      case 5 => (5 * bookPrice * 0.75)
+    }).toInt
+
+  case class Group(books: List[List[Int]])
+
+  def groupBooks(books: List[Int]): List[Group] = {
+    def internal(books: List[Int],
+                 sizeLimit: Int,
+                 acc: List[Int],
+                 remaining: List[Int],
+                 group: List[List[Int]]): Group = {
+      if ((books.isEmpty && remaining.isEmpty))
+        Group(group :+ acc)
+      else if (acc.size == sizeLimit)
+        internal(remaining ++ books,
+                 sizeLimit,
+                 List.empty,
+                 List.empty,
+                 group :+ acc)
+      else if (books.isEmpty)
+        internal(remaining, sizeLimit, List.empty, List.empty, group :+ acc)
+      else {
+        val head = books.head
+        if (!acc.contains(head))
+          internal(books.tail, sizeLimit, acc :+ head, remaining, group)
+        else
+          internal(books.tail, sizeLimit, acc, remaining :+ head, group)
+      }
+    }
+
+    List(
+      internal(books, 5, List.empty, List.empty, List.empty),
+      internal(books, 4, List.empty, List.empty, List.empty),
+      internal(books, 3, List.empty, List.empty, List.empty),
+      internal(books, 2, List.empty, List.empty, List.empty),
+      internal(books, 1, List.empty, List.empty, List.empty)
+    )
+  }
+
+}
