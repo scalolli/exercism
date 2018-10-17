e969a2479d67bdcca6791f9ebf5d4f06ef6e5271
Basavaraj Kalloli <scalolli@users.noreply.github.com>
2018-10-17 22:42:44 +0100

    refactored

--- scala/etl/src/main/scala/Etl.scala
+++ scala/etl/src/main/scala/Etl.scala
@@ -1,7 +1,7 @@
 object Etl {
   def transform(pointsToAplhabets: Map[Int, Seq[String]]): Map[String, Int] =
     pointsToAplhabets.flatMap {
-      case (key, value) =>
-        value.map(x => x.toLowerCase -> key)
+      case (key, values) =>
+        values.map(_.toLowerCase -> key)
     }
 }
