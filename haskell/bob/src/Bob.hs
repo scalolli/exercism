7f6c5755f4600b956f807a4c8efe5f81d3a243fa
Basavaraj Kalloli <scalolli@users.noreply.github.com>
2018-10-26 16:06:18 +0100

    Make it pointfree and move logic to endsWith func

--- haskell/bob/src/Bob.hs
+++ haskell/bob/src/Bob.hs
@@ -3,13 +3,6 @@ module Bob (responseFor) where
 import Data.Char    
 import Data.Semigroup
 
-responseFor :: String -> String
-responseFor xs = 
-    show $ case trim xs of
-            "" -> Fine 
-            trimmed -> endsWith trimmed
-
-
 data Reaction = Sure | ChillOut | CalmDown | Fine | Whatever deriving (Eq)
 
 instance Show Reaction where
@@ -21,16 +14,21 @@ instance Show Reaction where
             Fine -> "Fine. Be that way!"
             _    -> "Whatever."
 
+responseFor :: String -> String
+responseFor = show . endsWith . trim
+
 endsWith :: String -> Reaction    
-endsWith xs 
-    | (containsNumbers xs && (lastChar xs == '!')) = ChillOut
-    | (containsNumbers xs && (lastChar xs == '?')) = Sure
-    | (containsNumbers xs) = Whatever
-    | (allCaps xs) || (lastChar xs == '!' && allCaps (init xs)) = ChillOut      
-    | (lastChar xs == '?' && allCaps (init xs)) = CalmDown      
-    | otherwise = case (lastChar xs) of
-                    '?' -> Sure                    
-                    _   -> Whatever
+endsWith ys = 
+    if(ys == "") then Fine else go ys
+    where go xs 
+            | (containsNumbers xs && (lastChar xs == '!')) = ChillOut
+            | (containsNumbers xs && (lastChar xs == '?')) = Sure
+            | (containsNumbers xs) = Whatever
+            | (allCaps xs) || (lastChar xs == '!' && allCaps (init xs)) = ChillOut      
+            | (lastChar xs == '?' && allCaps (init xs)) = CalmDown      
+            | otherwise = case (lastChar xs) of
+                            '?' -> Sure                    
+                            _   -> Whatever
 
 allCaps :: String -> Bool
 allCaps = foldl (\b a -> if(a /= ' ') then b && (isUpper a) else b) True
