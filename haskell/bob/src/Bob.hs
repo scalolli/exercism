4ab19a98e3bd2090838b2ea500770e64b0d7ff3e
Basavaraj Kalloli <scalolli@users.noreply.github.com>
2018-10-26 15:47:28 +0100

    Implement the show type class and handle all the cases

--- haskell/bob/src/Bob.hs
+++ haskell/bob/src/Bob.hs
@@ -5,23 +5,32 @@ import Data.Semigroup
 
 responseFor :: String -> String
 responseFor xs = 
-    case (endsWith xs) of 
-        Sure -> "Sure."
-        ChillOut -> "Whoa, chill out!"
-        CalmDown -> "Calm down, I know what I'm doing!"
-        _    -> "Whatever."
+    show $ case trim xs of
+            "" -> Fine 
+            trimmed -> endsWith trimmed
 
-data Reaction = Sure | ChillOut | CalmDown | Fine | Whatever deriving (Eq, Show)
+
+data Reaction = Sure | ChillOut | CalmDown | Fine | Whatever deriving (Eq)
+
+instance Show Reaction where
+    show reaction = 
+        case reaction of 
+            Sure -> "Sure."
+            ChillOut -> "Whoa, chill out!"
+            CalmDown -> "Calm down, I know what I'm doing!"
+            Fine -> "Fine. Be that way!"
+            _    -> "Whatever."
 
 endsWith :: String -> Reaction    
 endsWith xs 
+    | (containsNumbers xs && (lastChar xs == '!')) = ChillOut
+    | (containsNumbers xs && (lastChar xs == '?')) = Sure
+    | (containsNumbers xs) = Whatever
     | (allCaps xs) || (lastChar xs == '!' && allCaps (init xs)) = ChillOut      
     | (lastChar xs == '?' && allCaps (init xs)) = CalmDown      
     | otherwise = case (lastChar xs) of
-                    '?' -> Sure
-                    '.' -> Whatever 
-                    '!' -> Whatever 
-                    _   -> Fine
+                    '?' -> Sure                    
+                    _   -> Whatever
 
 allCaps :: String -> Bool
 allCaps = foldl (\b a -> if(a /= ' ') then b && (isUpper a) else b) True
@@ -29,6 +38,12 @@ allCaps = foldl (\b a -> if(a /= ' ') then b && (isUpper a) else b) True
 lastChar :: String -> Char
 lastChar xs = (xs !! ((length xs) - 1))
 
--- anyCharIsCaps :: String -> Bool
--- anyCharIsCaps xs = getAny $ foldl (\b a -> Any (isUpper a) `mappend` b) (Any False) xs 
+containsNumbers :: String -> Bool 
+containsNumbers = foldl (\b a -> if(isNumber a) then True else b) False
+
+trim :: String -> String
+trim xs = dropSpace $ reverse $ dropSpace $ (reverse xs)
+
+dropSpace :: String -> String
+dropSpace = dropWhile isSpace
          
